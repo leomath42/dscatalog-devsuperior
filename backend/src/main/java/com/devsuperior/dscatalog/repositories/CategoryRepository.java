1d58d0bf448df38a2a513a04a4f31ea0e3a042f5
leomath42 <leonardomatheus042@gmail.com>
2021-06-07 20:07:11 -0300

    H2 database

---
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/CategoryRepository.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.devsuperior.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.devsuperior.dscatalog.entities.Category;
+
+/**
+ * Representa a camada de acesso a dados.
+ *
+ */
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -1,14 +1,16 @@
-package com.devsuperior.dscatalog.resource;
+package com.devsuperior.dscatalog.resources;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.services.CategoryService;
 
 /*
  * Representa um Controlador Rest. 
@@ -20,12 +22,12 @@ import com.devsuperior.dscatalog.entities.Category;
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 	
+	@Autowired
+	CategoryService categoryService;
+	
 	@GetMapping
 	public ResponseEntity<List<Category>> findAll(){
-		List<Category> list = new ArrayList<>();
-				
-		list.add(new Category(1L, "Books"));
-		list.add(new Category(2L, "Eletronics"));
+		List<Category> list = categoryService.findAll();
 		
 		return ResponseEntity.ok().body(list);
 	}
