c1b005e6a9791c52fd9ef137d018a4dbcd26e6a5
leomath42 <leonardomatheus042@gmail.com>
2021-06-10 21:18:38 -0300

    Product insert, updatte, delete

--- backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
@@ -25,14 +25,14 @@ public class ProductDTO implements Serializable{
 	public ProductDTO() {
 	}
 	
-//	public ProductDTO(String name, String description, Double price, String imgUrl, Instant date) {
-//		super();
-//		this.name = name;
-//		this.description = description;
-//		this.price = price;
-//		this.imgUrl = imgUrl;
-//		this.date = date;
-//	}
+	public ProductDTO(Long id, String name, String description, Double price, String imgUrl, Instant date) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
+		this.date = date;
+	}
 
 	public ProductDTO(Product entity) {
 		this.id = entity.getId();
