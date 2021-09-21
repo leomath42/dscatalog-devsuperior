cf02ea5413bc40b6806b3f86050868cf5534fffd
leomath42 <leonardomatheus042@gmail.com>
2021-09-21 20:10:43 -0300

    Custom ConstraintValidator

--- backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
@@ -1,21 +1,24 @@
 package com.devsuperior.dscatalog.dto;
 
+import com.devsuperior.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
-	
-	private static final long serialVersionUID = 1L;
-	
-	private String password;
-	
-	public UserInsertDTO() {
-		super();
-	}
 
-	public String getPassword() {
-		return password;
-	}
+    private static final long serialVersionUID = 1L;
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
+    private String password;
+
+    public UserInsertDTO() {
+        super();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
